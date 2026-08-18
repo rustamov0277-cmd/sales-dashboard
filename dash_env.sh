aaf871a773a66ef64cc1c300324fe86488ed203b
SinolifeSalesAdmin <rustamov0277-cmd@users.noreply.github.com>
2026-08-18 09:38:39 +0000

    Dashboard birinchi versiya

---
+++ dash_env.sh
@@ -0,0 +1,6 @@
+export SA_DASHBOARD_SALT="sinolife-2026-maxfiy-XyZ9kL3"
+export SA_STATE_DIR="/root/sinolifesalesadmin_v2/state"
+export SA_DASHBOARD_DIR="/root/sales_dashboard/docs"
+export SA_DASHBOARD_SALT="sinolife-2026-maxfiy-XyZ9kL3"
+export SA_STATE_DIR="/root/sinolifesalesadmin_v2/state"
+export SA_DASHBOARD_DIR="/root/sales_dashboard/docs"
